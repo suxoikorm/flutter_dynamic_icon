36f1a31427184de59278a2bea88acf05ddf620e2
Luka Katsadze <suxoikorm@gmail.com>
2025-05-28 02:04:02 +0400

    refactor: remove deprecated registerWith method from FlutterDynamicIconPlugin

--- android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/FlutterDynamicIconPlugin.java
+++ android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/FlutterDynamicIconPlugin.java
@@ -10,12 +10,6 @@ public class FlutterDynamicIconPlugin implements FlutterPlugin {
   private static final String CHANNEL_NAME = "flutter_dynamic_icon";
   private MethodChannel channel;
 
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    final FlutterDynamicIconPlugin plugin = new FlutterDynamicIconPlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
-  }
-
   @Override
   public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
     setupChannel(binding.getBinaryMessenger(), binding.getApplicationContext());
